3b6dcf257bda53aa41f5b2d7d4d59d466d96aa66
swas111 <92736155+swas111@users.noreply.github.com>
2021-10-27 22:02:23 +0530

    Update luckyarray.cpp

--- luckyarray.cpp
+++ luckyarray.cpp
@@ -1,85 +1,77 @@
 #include <iostream>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#define lowbit(x) (x & (-x))
-#define rep(i,a,b) for (int i=a;i<b;i++)
-#define repp(i,a,b) for (int i=a;i<=b;i++)
 using namespace std;
-typedef long long ll;
-const int INF = 1e9 + 10;
-const int MAXN = 1e5 + 5;
-const int MAXM = 1e4 + 5;
-const int mod = 1e9 + 7;
-const double EPS = 1e-10;
- 
+
 int n, m;
-int mark[MAXM], tree[MAXN], a[MAXN];
+int a[100005];
+int lucky[10005];
+int C[100005];
 
-void add(int x, int y)
+bool check(int n)
 {
-    while(x <= n){
-        tree[x] += y;
-        x += lowbit(x);
+    while (n)
+    {
+        if (n % 10 != 4 && n % 10 != 7) return 0;
+        n /= 10;
+    }
+    return 1;
+}
+
+void add(int pos, int t)
+{
+    while (pos <= n)
+    {
+        C[pos] += t;
+        pos += (pos & (-pos));
     }
 }
 
-int sum(int x)
+int sum(int pos)
 {
-    int tmp = 0;
-    while(x > 0){
-        tmp += tree[x];
-        x -= lowbit(x);
+    int ret = 0;
+    while (pos > 0)
+    {
+        ret += C[pos];
+        pos -= (pos & (-pos));
     }
-    return tmp;
-}
-
-void init()
-{
-    memset(mark, 0, sizeof mark);
-    mark[4]=mark[7]=mark[44]=mark[47]=1;
-    mark[74]=mark[77]=mark[444]=mark[447]=1;
-    mark[474]=mark[477]=mark[744]=mark[747]=1;
-    mark[774]=mark[777]=mark[4444]=mark[4447]=1;
-    mark[4474]=mark[4477]=mark[4744]=mark[4747]=1;
-    mark[4774]=mark[4777]=mark[7444]=mark[7447]=1;
-    mark[7474]=mark[7477]=mark[7744]=mark[7747]=1;
-    mark[7774]=mark[7777]=1;
+    return ret;
 }
 
 int main()
 {
-    int x, y, z;
-    char s[10];
-    init();
-    while(cin >> n >> m){
-        repp(i, 0, n)
-            tree[i] = 0;
-        repp(i, 1, n){
-            scanf("%d", a+i);
-            if(mark[a[i]])
-                add(i, 1);
-        }
-        rep(j, 0, m){
-            cin >> s >> x >> y;
-            if(s[0] == 'c')
-                cout << sum(y) - sum(x - 1) << endl;
-            else{
-                cin >> z;
-                if(z == 0)
-                    continue;
-                repp(i, x, y){
-                    if(mark[a[i]])
-                        add(i, -1);
-                    a[i] += z;
-                    if(mark[a[i]])
-                        add(i, 1);
-                }
+    for(int i = 1; i <= 10000; i++)
+    {
+        if (check(i))
+            lucky[i] = 1;
+    }
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (lucky[a[i]])
+            add(i, 1);
+    }
+    for(int i = 0; i < m; i++)
+    {
+        string ope;
+        int l, r;
+        cin >> ope >> l >> r;
+        if (ope == "add")
+        {
+            int k;
+            cin >> k;
+            for(int j = l; j <= r; j++)
+            {
+                int t = lucky[a[j]] ? -1 : 0;
+                a[j] += k;
+                if (lucky[a[j]]) t++;
+                if (t != 0) add(j, t);
             }
         }
+        else
+            cout << sum(r) - sum(l - 1) << endl;
     }
+
     return 0;
 }
 
-		  	 		 			 		 	   	 	 	 	 		
+ 						 	 	 	 			   		   	
